f7cf3ed35ed3fddadbe2be9ae45f945012000de9
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-01-22 11:51:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.Prefix = `!`;
+exports.Color = `#7289DA`;
+exports.Token = `OTI5MjY0NDg5NzIxOTIxNTc3.Ydky_w.n0dmAjAUTaPoAdvJkyH23wtbJdM`
+// Không nên để lộ Token nếu không bot sẽ phản chủ đó
