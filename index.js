47f5b674d25ebcb753a8b3b0f0830bf9dbca0355
Ân <91877913+MorgTheC@users.noreply.github.com>
2022-01-22 11:46:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require("express"); /*This is only for replit 
+*/
 const app = express();
 
 app.listen(3000, () => {
@@ -8,13 +9,13 @@ app.listen(3000, () => {
 
 app.get("/", (req , res) => {
   res.send("hi")
-})
+}) 
+//If you are using Visual Studio Code try https://railway.app
+const { Client, Intents } = require('discord.js'); //Require the discord.js package
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); // Require intents for discord API
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.on("messageCreate", message => {
-  if(message.content === ("!ping") {
+client.on("messageCreate", message => { //The messageCreate is require if your bot need to send embed message
+  if(message.content === ("!ping") { 
      message.channel.send("pong")
   }
 })
